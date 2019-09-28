61ba03958d604a7647f0bfa0d4684d444bb8740b
Susmit141 <52137646+Susmit141@users.noreply.github.com>
2019-09-28 14:10:56 +0530

    Add files via upload

---
+++ temp.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+This is yur python IDE (Search what IDE means):
+    Its like a python environment who you can run programs!
+    Run file rusn anything that you write here
+      Try looking for Spyder tutorials
