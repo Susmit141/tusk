40f3a1eb313d706a3465445d812c46b727309c54
Susmit141 <52137646+Susmit141@users.noreply.github.com>
2019-09-30 23:30:31 +0530

    Add files via upload

---
+++ temp 1.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+This is yur python IDE (Search what IDE means):
+    Its like a python environment who you can run programs!
+    Run file rusn anything that you write here
+def      Try looking for Spyder tutorials
+def is_palindrome_v1(s):
+    """ (str) -> bool
+    Return True if and only if s is a palindrome.
+    
+    
