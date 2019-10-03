efa7edf469c36d20b2a1beb4668ea626bf1ddc4f
Susmit141 <52137646+Susmit141@users.noreply.github.com>
2019-10-03 18:13:03 +0530

    Add files via upload

--- pf.py
+++ pf.py
@@ -1,21 +1,7 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri Sep 27 20:20:52 2019
+Created on Thu Oct  3 02:18:00 2019
 
 @author: SUSHMIT
 """
-n = int(input("enter an integer"))
-print("factors are")
-i = 1
-while(i<=n):
-    k = 0
-    if(n%i==0):
-        j = 1
-        while(j<=i):
-            if(i%j==0):
-                k = k+1
-                j = j+1
-                if(k==2):
-                    print(i)
-                    i=i+1
-                    
+
